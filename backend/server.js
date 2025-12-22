7eb2c91d3d77a859f9914e6e9b57695e43d28f88
“Samuel” <samuelmwaura081@gmail.com>
2025-12-22 20:35:17 +0300

    Committing changes to files after firing up the FE and BE servers

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use(cors());
 app.use('/api/products', productRoute)
 
 // Define the PORT
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT}`)
